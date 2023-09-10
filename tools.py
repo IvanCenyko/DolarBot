589c1c9ef23fff4f4cb1b40110ab6340fb8644e6
IvanCenyko <ivancenyko@gmail.com>
2023-09-10 22:45:16 +0100

    performance enhanced

--- tools.py
+++ tools.py
@@ -36,30 +36,13 @@ def json_lector(direction:str):
     
 def blue(save_dir):
     # request
-    dolar_request = requests.get('https://dolarhoy.com/')
-    # html crudo
-    soup = BeautifulSoup(dolar_request.content, 'html.parser')
-    # divs con clase tile is-child
-    divs = soup.find_all(class_='tile is-child')
-    # primer resultado, es decir, dolar blue
-    div_dolar = divs[0]
-    # busco en la div del blue la parte de venta
-    venta_soup = div_dolar.find(class_='venta')
-    # busco en la parte de venta el precio, y filtro solo texto
-    venta = venta_soup.find(class_='val').text
-
-    # repito ultimos dos pasos pero con compra
-    compra_soup = divs[0].find(class_='compra')
-    compra = compra_soup.find(class_='val').text
-
-    # quito el '$' y convierto a int
-    venta = int(venta.replace('$', ''))
-    compra = int(compra.replace('$', ''))
-
+    dolar = requests.get('https://apiweb-ivancenyko.vercel.app/dolar-blue').json()
     try:
         json_borrador(save_dir)
     except:
         pass
+    compra = dolar["compra"]
+    venta =  dolar["venta"]
     json_escritor(direction=save_dir, dict={"venta": venta, "compra": compra})
     return {"venta": venta, "compra": compra}
 
